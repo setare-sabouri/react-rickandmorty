1e25dbc56ffd169974db7506488b8a747bc8844c
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2022-12-16 17:40:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
 
 function App() {
+  let apiUrl = `https://rickandmortyapi.com/api/character/?page=${1}`;
+  let [fetchedData, updateFetchedData] = useState([]);
+  console.log(fetchedData);
+  useEffect(() => {
+    (async function () {
+      let src = await fetch(apiUrl);
+      let data = await src.json();
+      updateFetchedData(data);
+    })();
+  }, [apiUrl]); //watch on apiUrl
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-wrapper">
+      <nav className="navbar">navbar</nav>
+      <main className="container">
+        <h1 className="text-center mb-3">Characters</h1>
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">Card component will be placed here</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
