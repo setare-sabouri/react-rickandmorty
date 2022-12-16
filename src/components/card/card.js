9124ed6e81fe01f43a7794aca2ac27452446716b
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2022-12-16 21:32:42 +0100

    rendering raw cards

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,5 +1,26 @@
+import "./card.css";
 const Card = ({ characters }) => {
-  console.log(characters);
+  let cards;
+  if (characters) {
+    cards = characters.map((item) => {
+      let { id, image, name, status, location } = item;
+
+      return (
+        <div
+          key={id}
+          className=" card-container col-lg-4 col-md-6 col-sm-6 col-12 p-2"
+        >
+          <img className="card-img" src={image} alt=""></img>
+          <p>{name}</p>
+          <p>last seen: {location.name}</p>
+          <p>{status}</p>
+        </div>
+      );
+    });
+  } else {
+    cards = "No cards found !";
+  }
+  return <>{cards}</>;
 };
 
 export default Card;
